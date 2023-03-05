8945edf3b85bb9b2bf7f8d4deb273517d419ab53
gregtom6 <gregtom6@gmail.com>
2023-03-05 16:43:14 +0100

    unit creation and position filling

--- Illuv_Un/Assets/Scripts/GridCreator.cs
+++ Illuv_Un/Assets/Scripts/GridCreator.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class GridCreator : MonoBehaviour
 {
     [SerializeField] GameObject gridElement;
@@ -11,10 +12,14 @@ public class GridCreator : MonoBehaviour
     [SerializeField] float gridNewDistanceFromPreviousLine = 8.5f;
     [SerializeField] float gridNewLineIntendation = -5f;
 
+    public List<Vector3> gridElementPositions = new List<Vector3>();
+
     // Start is called before the first frame update
     void Start()
     {
         Vector3 position = Vector3.zero;
+
+        gridElementPositions.Clear();
 
         for (int i = 0; i < horizontalCount; i++)
         {
@@ -22,10 +27,14 @@ public class GridCreator : MonoBehaviour
             {
                 Instantiate(gridElement, position, Quaternion.identity, transform);
 
+                gridElementPositions.Add(position + new Vector3(5f, 0f, 5f));
+
                 position = new Vector3(position.x, position.y, position.z + gridNewElementOffset);
             }
 
             position = new Vector3(position.x + gridNewDistanceFromPreviousLine, position.y, i % 2 == 0 ? gridNewLineIntendation : 0f);
         }
     }
+
+    
 }
---
+++ Illuv_Un/Assets/Scripts/UnitController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+    [SerializeField] float hexesPerTimeStep;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
